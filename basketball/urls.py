85f9ab6749d2dd9703f15583fd723c2de96c2448
niluws <ns1378@yahoo.com>
2024-03-14 17:50:54 +0330

    change viewsets to generics-add view: home_class

--- basketball/urls.py
+++ basketball/urls.py
@@ -1,19 +1,18 @@
-from rest_framework.routers import DefaultRouter
-
-from .views import NewsModelViewSet, ClassModelViewSet, ImageModelViewSet, StaffModelViewSet, BlogModelViewSet, \
-    AboutModelViewSet, LeagueModelViewSet, LeagueTableViewSet, BossModelViewSet, CommitteeModelViewSet
+from django.urls import path
+from . import views
 
 app_name = 'basketball'
-router = DefaultRouter()
-router.register(r'news', NewsModelViewSet, basename='news')
-router.register(r'class', ClassModelViewSet, basename='class')
-router.register(r'blog', BlogModelViewSet, basename='blog')
-router.register(r'image', ImageModelViewSet, basename='image')
-router.register(r'staff', StaffModelViewSet, basename='staff')
-router.register(r'boss', BossModelViewSet, basename='boss')
-router.register(r'committee', CommitteeModelViewSet, basename='committee')
-router.register(r'about', AboutModelViewSet, basename='about')
-router.register(r'league', LeagueModelViewSet, basename='league')
-router.register(r'league_table', LeagueTableViewSet, basename='league_table')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('news/', views.NewsModelListAPI.as_view(), name='news'),
+    path('class/', views.ClassModelListAPI.as_view(), name='class'),
+    path('home_class/', views.HomeClassListAPI.as_view(), name="home_class"),
+    path('blog/', views.BlogModelListAPI.as_view(), name='blog'),
+    path('image/', views.ImageModelListAPI.as_view(), name='image'),
+    path('staff/', views.StaffModelListAPI.as_view(), name='staff'),
+    path('boss/', views.BossModelListAPI.as_view(), name="boss"),
+    path('committee/', views.CommitteeModelListAPI.as_view(), name="committee"),
+    path('about/', views.AboutModelListAPI.as_view(), name="about"),
+    path('league/', views.LeagueModelListAPI.as_view(), name="league"),
+    path('league_table/', views.LeagueTableListAPI.as_view(), name="league_table"),
+]
--- basketball/views.py
+++ basketball/views.py
@@ -1,72 +1,65 @@
-from rest_framework import viewsets
+from rest_framework import generics
 
 from .models import NewsModel, ClassModel, ImagesModel, StaffModel, BlogModel, LeagueTableModel, LeagueModel, \
-    AboutModel, BossModel, CommitteeModel
+    AboutModel, BossModel, CommitteeModel, ClassDetailModel
 from .serializers import NewsModelSerializer, ClassModelSerializer, ImageModelSerializer, StaffModelSerializer, \
     BlogModelSerializer, AboutModelSerializer, LeagueModelSerializer, LeagueTableSerializer, BossModelSerializer, \
-    CommitteeModelSerializer
+    CommitteeModelSerializer, ClassDetailModelSerializer
 
 
-class NewsModelViewSet(viewsets.ModelViewSet):
+class NewsModelListAPI(generics.ListAPIView):
     queryset = NewsModel.objects.all()
     serializer_class = NewsModelSerializer
-    http_method_names = ['get']
 
 
-class ClassModelViewSet(viewsets.ModelViewSet):
+class ClassModelListAPI(generics.ListAPIView):
     queryset = ClassModel.objects.all()
     serializer_class = ClassModelSerializer
-    http_method_names = ['get']
 
 
+class HomeClassListAPI(generics.ListAPIView):
+    queryset = ClassDetailModel.objects.filter(show=True)
+    serializer_class = ClassDetailModelSerializer
 
-class BlogModelViewSet(viewsets.ModelViewSet):
+
+class BlogModelListAPI(generics.ListAPIView):
     queryset = BlogModel.objects.all()
     serializer_class = BlogModelSerializer
-    http_method_names = ['get']
 
 
-class ImageModelViewSet(viewsets.ModelViewSet):
+class ImageModelListAPI(generics.ListAPIView):
     queryset = ImagesModel.objects.all()
     serializer_class = ImageModelSerializer
-    http_method_names = ['get']
 
 
-# todo all members order by alphabet
 # todo Committee shows by genders{roll{gender{name and image--mabe change the table
-class StaffModelViewSet(viewsets.ModelViewSet):
+class StaffModelListAPI(generics.ListAPIView):
     queryset = StaffModel.objects.all()
     serializer_class = StaffModelSerializer
-    http_method_names = ['get']
 
 
-class BossModelViewSet(viewsets.ModelViewSet):
-    queryset = BossModel.objects.all()
+class BossModelListAPI(generics.ListAPIView):
+    queryset = BossModel.objects.all().order_by('full_name')
     serializer_class = BossModelSerializer
-    http_method_names = ['get']
 
 
-class CommitteeModelViewSet(viewsets.ModelViewSet):
-    queryset = CommitteeModel.objects.all()
+class CommitteeModelListAPI(generics.ListAPIView):
+    queryset = CommitteeModel.objects.all().order_by('full_name')
     serializer_class = CommitteeModelSerializer
-    http_method_names = ['get']
 
 
-class AboutModelViewSet(viewsets.ModelViewSet):
+class AboutModelListAPI(generics.ListAPIView):
     queryset = AboutModel.objects.all()
     serializer_class = AboutModelSerializer
-    http_method_names = ['get']
 
     # todo get just the last about
 
 
-class LeagueModelViewSet(viewsets.ModelViewSet):
+class LeagueModelListAPI(generics.ListAPIView):
     queryset = LeagueModel.objects.all()
     serializer_class = LeagueModelSerializer
-    http_method_names = ['get']
 
 
-class LeagueTableViewSet(viewsets.ModelViewSet):
+class LeagueTableListAPI(generics.ListAPIView):
     queryset = LeagueTableModel.objects.all()
     serializer_class = LeagueTableSerializer
-    http_method_names = ['get']
